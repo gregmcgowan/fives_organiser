52fced616c4dfa3f47668504d8101898d8c2df85
Greg McGowan <gregmcgowan@gmail.com>
2017-02-26 13:07:01 +0000

    Tidy up PlayerListView

--- app/src/main/java/com/gregmcgowan/fivesorganiser/players/PlayerListView.kt
+++ app/src/main/java/com/gregmcgowan/fivesorganiser/players/PlayerListView.kt
@@ -1,7 +1,6 @@
 package com.gregmcgowan.fivesorganiser.players
 
 import android.content.Context
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -12,50 +11,31 @@ import com.gregmcgowan.fivesorganiser.R
 
 class PlayerListView : FrameLayout, PlayerListContract.View {
 
-    var rootView: ViewGroup? = null
-
-    var progressBar: ProgressBar? = null
-
-    var playerList: RecyclerView? = null
-
+    val rootView: ViewGroup
+    val progressBar: ProgressBar
+    val playerList: RecyclerView
     val playerListAdapter: PlayerListAdapter = PlayerListAdapter()
 
-    constructor(context: Context?) : super(context) {
-        init()
-    }
+    constructor(context: Context?) : this(context, null)
 
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
-        init()
-    }
-
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init()
-    }
-
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
-        init()
-    }
-
-    private fun init() {
         rootView = LayoutInflater.from(context).inflate(R.layout.players_list, this, false) as ViewGroup
         addView(rootView)
-        progressBar = rootView?.findViewById(R.id.progress_bar) as ProgressBar?
-        playerList = rootView?.findViewById(R.id.player_list) as RecyclerView?
-        playerList?.layoutManager = LinearLayoutManager(playerList?.context)
-        playerList?.adapter = playerListAdapter
+        progressBar = rootView.findViewById(R.id.progress_bar) as ProgressBar
+        playerList = rootView.findViewById(R.id.player_list) as RecyclerView
+        playerList.adapter = playerListAdapter
     }
-
 
     override fun showPlayers(players: List<Player>) {
         playerListAdapter.setPlayers(players)
     }
 
     override fun showProgressBar(show: Boolean) {
-        progressBar?.visibility = if (show) VISIBLE else GONE
+        progressBar.visibility = if (show) VISIBLE else GONE
     }
 
     override fun showPlayerList(show: Boolean) {
-        playerList?.visibility = if (show) VISIBLE else GONE
+        playerList.visibility = if (show) VISIBLE else GONE
     }
 }
 
