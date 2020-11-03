20df6d7584f294a9ce3a44bffabe8845aa67550f
gregmcgowan <gregmcgowan@gmail.com>
2020-11-03 22:04:22 +0000

    Use gpg

--- signing/setup.sh
+++ signing/setup.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-ENCRYPT_KEY=$1
+#!/bin/bash
 
 # Decrypt Google Services key
-openssl enc -aes-256-cbc -d -in signing/google-services.aes -out app/google-services.json -k $ENCRYPT_KEY
+gpg --quiet --batch --yes --decrypt --passphrase=$GOOGLE_SERVICES_KEY --output app/google-services.json signing/google-services.json.gpg
