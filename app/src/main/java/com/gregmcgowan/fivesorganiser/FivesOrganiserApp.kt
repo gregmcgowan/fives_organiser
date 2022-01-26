1bbdfa1b4d9219a99f0a4c26f78f0160381da01e
gregmcgowan <gregmcgowan@gmail.com>
2022-01-26 14:19:24 +0000

    Remove some more unused dependencies

--- app/src/main/java/com/gregmcgowan/fivesorganiser/FivesOrganiserApp.kt
+++ app/src/main/java/com/gregmcgowan/fivesorganiser/FivesOrganiserApp.kt
@@ -2,7 +2,6 @@ package com.gregmcgowan.fivesorganiser
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
-import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -14,7 +13,6 @@ class FivesOrganiserApp : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-        AndroidThreeTen.init(this)
         FirebaseApp.initializeApp(this)
     }
 
