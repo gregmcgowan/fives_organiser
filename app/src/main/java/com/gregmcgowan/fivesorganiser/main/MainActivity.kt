fc89255816ea966bf72f28692e5403c65fb17b11
Greg McGowan <gregmcgowan@gmail.com>
2026-01-30 16:11:46 +0000

    Fix KtLint

--- app/src/main/java/com/gregmcgowan/fivesorganiser/main/MainActivity.kt
+++ app/src/main/java/com/gregmcgowan/fivesorganiser/main/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.gregmcgowan.fivesorganiser.main
 
 import android.os.Bundle
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.toArgb
 import com.gregmcgowan.fivesorganiser.core.compose.AppTheme
-import com.gregmcgowan.fivesorganiser.core.compose.Green_500
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
