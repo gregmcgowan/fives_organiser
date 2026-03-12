b8d887cfc16ccfa5373a005c62cd0349af3676c9
Greg McGowan <gregmcgowan@gmail.com>
2026-03-12 18:35:02 +0000

    Removing authentication

--- libraries/authentication-contract/src/main/java/com/gregmcgowan/fivesorganiser/authentication/Authentication.kt
+++
@@ -1,9 +0,0 @@
-package com.gregmcgowan.fivesorganiser.authentication
-
-interface Authentication {
-    suspend fun initialise()
-
-    fun isInitialised(): Boolean
-
-    fun getUserId(): String
-}
